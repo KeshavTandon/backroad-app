1c68ddc34b4bbf64aaa774741c50babdaf9943a1
KeshavTandon <keshavtandon51@gmail.com>
2023-12-11 16:02:46 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import Services from "./components/Services"
+import Home from "./components/Home"
+import About from "./components/About"
+import Tours from "./components/Tours"
+import Footer from "./components/Footer"
+import Name from "./components/Name"
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+    <Name/>
+    </>
+  )
+}
+export default App
---
+++ src/components/Name.jsx
@@ -0,0 +1,8 @@
+const Name = () => {
+  return (
+    <center>
+        <h4>Keshav Tandon , 2100320120090</h4>
+    </center>
+  )
+}
+export default Name
